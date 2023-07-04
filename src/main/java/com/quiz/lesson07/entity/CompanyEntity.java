30e631df245f04f5f1563313aeaca51316cb61e3
test <test@test.com>
2023-07-04 18:43:24 +0900

    0704 수업

---
+++ src/main/java/com/quiz/lesson07/entity/CompanyEntity.java
@@ -0,0 +1,44 @@
+package com.quiz.lesson07.entity;
+
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name= "company")
+public class CompanyEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	private String business;
+	private String scale;
+	private int headcount;
+	
+	@UpdateTimestamp
+	@Column(name = "createdAt", updatable = false)
+	private ZonedDateTime createdAt;
+	
+	@UpdateTimestamp
+	@Column(name = "updatedAt")
+	private ZonedDateTime updatedAt;
+}
